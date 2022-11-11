51cd145036804cef8613facece05cd590d0a88f6
root <m.buldo@alustudent.com>
2022-11-11 09:52:16 -0800

    task done

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+
+a class 'square' that defines a square given instructions
+
+"""
+
+
+class Square:
+    """
+    square with a private instance attribute: size
+    
+    """
+
+    def __init__(self, size):
+        """
+        new square size
+
+        """
+        self.__size = size
