813d706718f6acdca42cefa0615a383bc61db1c1
root <m.buldo@alustudent.com>
2022-11-11 11:28:19 -0800

    task done

---
+++ python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+create square
+
+"""
+
+
+class Square:
+    """
+    create square with private instance att.: size
+
+    """
+
+    def __init__(self, size=0):
+        """
+        gives current square size
+
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """must proof size to be an integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Return: area of the square
+
+        """
+        return (self.__size * self.__size)
