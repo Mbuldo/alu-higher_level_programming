c183065b82298b9da79bf07d2bbbe44847154875
root <m.buldo@alustudent.com>
2022-11-11 14:21:32 -0800

    task done

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -31,7 +31,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
     def area(self):
         """print area of square"""
         square_area = self.__size ** 2
